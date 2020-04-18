517b6530c663ed3585a26600926839d8600227c6
usr_gsm <cgarciam@tigo.com.gt>
2020-04-18 13:55:41 -0600

    Added scene 1 with basic movement with two different positions.

---
+++ 01-basic-movement/01 Basic Development/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public int distance;
+
+    void Update()
+    {
+        this.transform.position += this.transform.forward * this.speed * Time.deltaTime;
+
+        if (this.transform.position.z >= this.distance || this.transform.position.z <= 0)
+        {
+            this.speed *= -1;
+        }
+    }
+}
